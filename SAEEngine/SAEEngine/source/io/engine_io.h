569233139c1b6df3dc19a2045651dfc164b2a4a4
JonathanCline <59320680+JonathanCline@users.noreply.github.com>
2020-10-13 22:04:47 -0500

    Outlined standardized io library

--- SAEEngine/SAEEngine/source/io/engine_io.h
+++ SAEEngine/SAEEngine/source/io/engine_io.h
@@ -9,6 +9,31 @@ namespace sae::engine
 	std::ostream* lua_getostream(lua_State* _lua);
 	std::istream* lua_getistream(lua_State* _lua);
 
+	struct lib_io
+	{
+	private:
+
+		// io.print(string)
+		static int print(lua_State* _lua);
+
+		// io.println(string)
+		static int println(lua_State* _lua);
+
+		// io.read(count) -> string
+		static int read(lua_State* _lua);
+
+		// io.readln() -> string
+		static int readln(lua_State* _lua);
+
+
+
+
+	public:
+
+		static int lua_open(lua_State* _lua);
+
+	};
+
 	// engine.io.print(string)
 	int io_print(lua_State* _lua);
 
